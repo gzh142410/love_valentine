8b7ce570df3850c6a6ce21060d671cc8958bd9a3
gzh142410 <nguyengiap2004bg@gmail.com>
2025-02-14 14:13:03 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/love-quest-joy/',
+  base: '/love_valentine/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
